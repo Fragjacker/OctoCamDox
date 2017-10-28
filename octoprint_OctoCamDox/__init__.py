502ec923926f2ed5c8da132b6944e3e935a413a7
FragJacker <fam.struhs@hamburg.de>
2017-10-28 14:22:02 +0200

    Updated Version and Readme file

--- octoprint_OctoCamDox/__init__.py
+++ octoprint_OctoCamDox/__init__.py
@@ -53,6 +53,7 @@ from .image_mergerWithOptions import ImageMergerWithOptions as imergeOpts
 
 
 __plugin_name__ = "OctoCamDox"
+__plugin_version__ = "0.9"
 
 #instantiate plugin object and register hook for gcode injection
 def __plugin_load__():
